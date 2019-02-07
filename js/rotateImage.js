0c2b3bb99c4def46ac5dbcc12bf7bc3bbfdb5df3
alejandrok93 <alejandrok93@gmail.com>
2019-02-06 18:21:33 -0800

    fixed bug with for loops

--- js/rotateImage.js
+++ js/rotateImage.js
@@ -7,7 +7,7 @@ function rotateImage(matrix) {
 	});
 
 	for (let i = 0; i < matrix.length; i++) {
-		for (let j = 0; j < matrix.length; j++) {
+		for (let j = 0; j < i; j++) {
 			[matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
 		}
 	}
