cbe681a87c806c3974909d33feddbf4a15de9ee4
alejandrok93 <alejandrok93@gmail.com>
2019-02-06 18:11:48 -0800

    Add for loops

--- js/rotateImage.js
+++ js/rotateImage.js
@@ -1,3 +1,10 @@
+// Given an image represented by an NxN matrix, where each pixel in the image is an integer from 0 to 9, write a function rotateImage that rotates the image by 90 degrees in the counter-clockwise direction.
+
 function rotateImage(arr) {
 	//rorate image
+	for (let row of arr) {
+		for (let i = 0; i < row.length; i++) {}
+	}
 }
+
+[[1, 2], [3, 4]];
