94f64b2518cfe80eb175a7e025c90eb53ef831c4
alejandrok93 <alejandrok93@gmail.com>
2019-02-06 18:18:53 -0800

    updated with model solution

--- js/rotateImage.js
+++ js/rotateImage.js
@@ -1,10 +1,18 @@
 // Given an image represented by an NxN matrix, where each pixel in the image is an integer from 0 to 9, write a function rotateImage that rotates the image by 90 degrees in the counter-clockwise direction.
 
-function rotateImage(arr) {
-	//rorate image
-	for (let row of arr) {
-		for (let i = 0; i < row.length; i++) {}
+function rotateImage(matrix) {
+	//rotate image
+	matrix.forEach(row => {
+		row.reverse();
+	});
+
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix.length; j++) {
+			[matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+		}
 	}
+
+	return matrix;
 }
 
-[[1, 2], [3, 4]];
+console.log(rotateImage([[1, 2], [3, 4]]));
