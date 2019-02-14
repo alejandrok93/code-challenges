f43658754e352a3c0a718686dd0a0f8f7dc1b74f
alejandrok93 <alejandrok93@gmail.com>
2019-02-14 13:47:00 -0800

    BST from array, WIP

--- js/bstFromArray.js
+++ js/bstFromArray.js
@@ -10,8 +10,19 @@ class BinaryTreeNode {
 function bstFromAray(arr) {
 	//get root node in the middle of array
 	let rootItem = Math.floor(arr.length / 2);
+	root = BinaryTreeNode(arr[rootItem]);
 	let left = arr.slice(0, rootItem - 1);
 	let right = arr.slice(rootItem + 1, arr.length);
 	//loop to the left and position elements to its left side
+	for (let i = left.length - 1; i >= 0; i--) {
+		if (arr[i] > root.value) {
+			root.right = arr[i];
+		} else if (arr[i] < root.value) {
+			root.left = arr[i];
+		}
+	}
 	//loop to the right and position elements to its right side
 }
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(bstFromArray(arr));
