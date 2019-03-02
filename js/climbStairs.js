936a24f572e5c99e3922b636f35b6323950b8c62
alejandrok93 <alejandrok93@gmail.com>
2019-03-01 16:55:34 -0800

    set up base cases for climb stairs challenge

--- js/climbStairs.js
+++ js/climbStairs.js
@@ -1,3 +1,18 @@
-function climbStairs(n) {}
+function climbStairs(n) {
+	//set up base cases
+	if (n === 0) {
+		return 1;
+	}
+	if (n === 1) {
+		return 1;
+	}
+	if (n === 2) {
+		return 2;
+	}
+	if (n === 3) {
+		return 4;
+	}
+	//recursive return statement
+}
 
 console.log(climbStairs(3)); // should print 4
