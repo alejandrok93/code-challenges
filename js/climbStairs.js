d6677b9d4db6f4b4f794b6bed798cdf6568ed3a5
alejandrok93 <alejandrok93@gmail.com>
2019-03-01 16:56:24 -0800

    completed climb stairs with dynamic programming solution

--- js/climbStairs.js
+++ js/climbStairs.js
@@ -1,4 +1,4 @@
-function climbStairs(n) {
+function climbStairs(n, cache = {}) {
 	//set up base cases
 	if (n === 0) {
 		return 1;
@@ -13,6 +13,17 @@ function climbStairs(n) {
 		return 4;
 	}
 	//recursive return statement
+	//dynamic programming hash table(?)
+	if (cache[n]) {
+		return cache[n];
+	}
+	//recursive return statement
+	cache[n] =
+		climbStairs(n - 1, cache) +
+		climbStairs(n - 2, cache) +
+		climbStairs(n - 3, cache);
+
+	return cache[n];
 }
 
 console.log(climbStairs(3)); // should print 4
