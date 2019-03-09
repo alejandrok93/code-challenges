f794ca459d781cffea0bc190a379ad18e932ac48
alejandrok93 <alejandrok93@gmail.com>
2019-03-09 07:10:35 -0800

    Added pseudocode for MaxProfit code challenge

--- js/bitcoinTrading.js
+++ js/bitcoinTrading.js
@@ -1,6 +1,15 @@
 // You're trying to make your fortune by trading Bitcoin. Suppose you want to automate this task. Write a function findMaxProfit that receives an array of today's Bitcoin prices. Your function should return the maximum profit you can make from a single buy and sell. You must buy first before selling; shorting is not allowed.
 function findMaxProfit(prices) {
 	//write code
+	let maxProfit = 0;
+
+	//loop through prices
+
+	//subtract previous price from current price to get current profit
+
+	// compare current profit with max profit
+
+	// if greater, update maxprofit
 }
 
 const bitcoinPrices = [1050, 270, 1540, 3800, 2];
