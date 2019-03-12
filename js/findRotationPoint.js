2fcc75070466a51541e38ffd1a51b12dc6ac0f91
alejandrok93 <alejandrok93@gmail.com>
2019-03-11 20:32:09 -0700

    Added Find Rotation Point code challenge

---
+++ js/findRotationPoint.js
@@ -0,0 +1,21 @@
+// You went and bought a copy of Webster's English dictionary on April Fool's Day and received the joke edition of the dictionary. You open it up and see that the first entry in the dictionary starts somewhere in the middle. Once the end of the alphabet is reached, it circles around and starts over at the 'A' section and goes all the way through until it reaches the beginning. Otherwise, though, everything else seems to be in order.
+
+// Write a function findRotationPoint that receives an array of words and calculates the index of the point where the rotation occurs. With the above list of words, your function would return 5.
+
+function findRotationPoint(words) {
+	//write code
+}
+
+const words = [
+	'ptolemaic',
+	'retrograde',
+	'supplant',
+	'undulate',
+	'xenoepist',
+	'asymptote', // <-- rotation happens here
+	'babka',
+	'banoffee',
+	'engender',
+	'karpatka',
+	'othellolagkage'
+];
