5b6cfb61756d82ec3cc3b00b68b5e72db0b5dd88
alejandrok93 <alejandrok93@gmail.com>
2019-03-27 10:53:29 -0700

    Added span to move from one column to the other

--- react/board/src/components/Card.js
+++ react/board/src/components/Card.js
@@ -3,10 +3,12 @@ import React from 'react';
 const Card = props => {
 	return (
 		<div className="card">
+			<span> back </span>
 			<p>{props.card.text}</p>{' '}
 			<span onClick={e => props.deleteCard(props.columnId, props.card.id)}>
 				delete
 			</span>
+			<span> next </span>
 		</div>
 	);
 };
