d9b67af442945e113567c873c85dc9bb65c5ff82
alejandrok93 <alejandrok93@gmail.com>
2019-04-02 09:34:15 -0700

    Added Result component and connected to DragSource

---
+++ react/giphy-collections/src/components/Result.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { DragSource } from 'react-dnd';
+
+const itemSource = {
+	beginDrag(props) {
+		console.log('dragging item');
+		return props.item;
+	},
+	endDrag(props, monitor, components) {
+		return props.handleDrop(props.item.id);
+	}
+};
+
+function collect(connect, monitor) {
+	return {
+		connectDragSource: connect.dragSource(),
+		connectDragPreview: connect.dragPreview(),
+		isDragging: monitor.isDragging()
+	};
+}
+
+class Result extends React.Component {
+	render() {
+		const { isDragging, connectDragSource, item } = this.props;
+		const opacity = isDragging ? 0 : 1;
+		return connectDragSource(
+			<div className="item" style={{ opacity }}>
+				<span>item.name</span>
+			</div>
+		);
+
+		// return <div className="item">{this.props.item.name}</div>;
+	}
+}
+
+export default DragSource('item', itemSource, collect)(Result);
