5441376480a73dcb2abefb116b595e4a96595ae6
alejandrok93 <alejandrok93@gmail.com>
2019-04-02 09:17:03 -0700

    Added html structure of app

--- react/giphy-collections/src/App.js
+++ react/giphy-collections/src/App.js
@@ -5,8 +5,19 @@ import './App.css';
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<h1>Giphy Collections</h1>
+			<div className="container">
+				<header className="header">
+					<h1>Giphy Collections</h1>
+				</header>
+				<div className="app">
+					<section className="collections-container">
+						{/* {<Collection />} */}
+					</section>
+					<section className="giphy-app">
+						<div className="search-container">{/* <Search /> */}</div>
+						<div className="giphy-container">{/* <Result /> */}</div>
+					</section>
+				</div>
 			</div>
 		);
 	}
