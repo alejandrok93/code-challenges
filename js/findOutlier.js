c9a7694222162cdf61947dd3dace5e6e7eeecd82
alejandrok93 <alejandrok93@gmail.com>
2019-04-28 21:02:37 -0700

    Completed and passing tests

--- js/findOutlier.js
+++ js/findOutlier.js
@@ -1,5 +1,6 @@
 function isOddNum(num) {
-	return num % 2;
+	let result = num % 2;
+	return result;
 }
 
 //Need to figure out if set of integers are odd or even
@@ -8,19 +9,41 @@ function findOutlier(integers) {
 	let outlier = null;
 	let isOdd = false;
 
-	if (isOddNum(integers[0]) === 1 && isOddNum(integers[1] === 1)) {
+	let odds = integers.filter(integer => {
+		if (isOddNum(integer) === 1) {
+			return integer;
+		}
+	});
+
+	let evens = integers.filter(integer => {
+		if (integer === 0) {
+			return integer;
+		}
+
+		if (isOddNum(integer) === 0) {
+			return integer;
+		}
+	});
+
+	if (odds.length === 1) {
+		isOdd = false;
+	} else if (evens.length === 1) {
 		isOdd = true;
 	}
 
 	if (isOdd === true) {
-		console.log('contains odd numbers');
-	} else console.log('contains even numbers');
+		outlier = evens[0];
+	} else if (isOdd === false) {
+		outlier = odds[0];
+	}
+
+	console.log(outlier);
+	return outlier;
 }
 
+findOutlier([9, 3, 7, 6, 11, 13, 17]); // odd numbers, should return 6
 findOutlier([0, 1, 2]); // even numbers, should return 1
 findOutlier([1, 2, 3]); // odd numbers, should return 2
 findOutlier([2, 6, 8, 10, 3]); // even numbers, should return 3
 findOutlier([0, 0, 3, 0, 0]); // even numbers, should return 3
 findOutlier([1, 1, 0, 1, 1]); // odd numbers, should return 0
-
-console.log(isOddNum(3));
