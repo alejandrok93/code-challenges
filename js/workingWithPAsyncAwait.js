4564ba9d7b955ec1b584e9575240045c5644a5fa
alejandrok93 <alejandrok93@gmail.com>
2019-05-09 10:12:32 -0700

    Converted axios request to async/await

--- js/workingWithPAsyncAwait.js
+++ js/workingWithPAsyncAwait.js
@@ -1,3 +1,12 @@
-function getPhotos() {
-	console.log('hi');
+const axios = require('axios');
+
+async function getPhotos() {
+	let url = 'https://picsum.photos/v2/list';
+	console.log('lets get these photos!');
+
+	const photos = await axios.get(url);
+	console.log('got them!');
+	console.log(photos);
 }
+
+console.log(getPhotos());
